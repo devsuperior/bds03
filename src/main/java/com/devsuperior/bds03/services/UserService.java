8e5470c6b4587b65c18a6e54f5fd9bc8f9ca1833
Nelio Alves <acenelio@gmail.com>
2021-05-23 18:01:30 -0300

    Final

---
+++ src/main/java/com/devsuperior/bds03/services/UserService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.bds03.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds03.entities.User;
+import com.devsuperior.bds03.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + username);
+		return user;
+	}
+}
